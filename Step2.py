46c4fd0e4eb7b422b006924deb0cf414e5b0d90b
janukem <janukem@due.com>
2016-08-17 20:06:18 -0400

    Step 2 done.

---
+++ Step2.py
@@ -0,0 +1,24 @@
+import requests
+
+#Set key values
+data = {
+	"token": "7fa0189d76ec2f2e1107a119c6c2f614",
+}
+
+#Make the request
+apiRequest = requests.post("http://challenge.code2040.org/api/reverse", data )
+
+#Flip the response
+reversedWord = apiRequest.text[::-1]
+
+#See if it worked
+print(apiRequest.text)
+print(reversedWord)
+
+newData = {
+	
+	"token": "7fa0189d76ec2f2e1107a119c6c2f614",
+	"string": reversedWord 
+}
+#Send it back
+newApiRequest = requests.post("http://challenge.code2040.org/api/reverse/validate", newData)
